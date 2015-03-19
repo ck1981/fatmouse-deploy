3add2a18fe441caab669751653b208f433de2b2b
Roman Bezruchko <r.bezruchko@scalr.com>
2015-03-19 15:40:12 +0300

    fix

--- cluster/bootstrap.sh
+++ cluster/bootstrap.sh
@@ -1,4 +1,6 @@
-gcloud preview container clusters create cluster-test --cluster-api-version 0.12 --machine-type f1-micro --num-nodes 2
+#!/bin/bash
+
+gcloud preview container clusters create cluster-test --cluster-api-version 0.12.0 --machine-type f1-micro --num-nodes 2
 gcloud preview container kubectl create --validate -f api-service.json
 gcloud preview container kubectl create --validate -f screen-service.json
 gcloud preview container kubectl create --validate -f worker-service.json
